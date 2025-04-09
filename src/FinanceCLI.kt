134c0589f79638ea018987d3d75ee4da93b60f0b
=Muhammed Wael <=mw26200400@gmail.com>
2025-04-09 15:18:30 +0200

    add categories Menu

--- src/FinanceCLI.kt
+++ src/FinanceCLI.kt
@@ -3,14 +3,14 @@ class FinanceCLI(
     fun start(){
         while (true){
             print("Welcome to PFT, How i can help you today\n" +
-                    "1- Income\n" +
+                    "1- Income\n" + // done
                     "2- Expenses\n" +
-                    "3- Categories\n" +
+                    "3- Categories\n" + // done
                     "4- View transaction\n" +
                     "5- Exit")
             when (readln()) {
                 "1" -> incomeMenu()
-                "2" -> expencesMenu()
+                "2" -> expensesMenu()
                 "3" -> categoriesMenu()
                 "4" -> viewTransaction()
                 "5" -> break
@@ -47,11 +47,41 @@ class FinanceCLI(
     }
 
     private fun categoriesMenu() {
+        print("---------------------------\n" +
+                "Categories Menu\n" +
+                "Here is all the Categories:\n")
+
+        // view all Categories
+        print("What do you want to do?\n" +
+                "1. Add\n" +
+                "2. Edit\n" +
+                "3. Delete\n" +
+                "3. Back\n" +
+                "Enter Your Option: ")
+        when (readln()){
+            "1" -> addCategory()
+            "2" -> editCategory()
+            "3" -> deleteCategory()
+            "4" -> start()
+            else -> println("Valid Input try again")
+        }
+    }
+    private fun addCategory() {
+
+    }
+    private fun deleteCategory() {
         TODO("Not yet implemented")
     }
 
-    private fun expencesMenu() {
+    private fun editCategory() {
         TODO("Not yet implemented")
+    }
+
+
+
+    private fun expensesMenu() {
+        print("---------------------------\n" +
+                "Expenses Menu\n")
     }
 
 
