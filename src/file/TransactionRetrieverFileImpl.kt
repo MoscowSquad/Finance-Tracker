23ba5c659d62994f5fc22de2a7571d0edfc5993a
alaafawzy <alaaf9443@gmail.com>
2025-04-09 22:12:15 +0200

    Implement TransactionRetrieverFile to retrieve, get all, and filter monthly transactions from file
    - Added getSpecificTransaction to find a transaction by its ID.
    - Added getAllTransaction to retrieve all transactions.
    - Added getMonthlyTransactions to filter transactions by month and sort them by date.

---
+++ src/file/TransactionRetrieverFileImpl.kt
@@ -0,0 +1,27 @@
+package file
+
+import Transaction
+import java.io.File
+
+class TransactionRetrieverFileImpl( storagePath: String = "data/transactions.csv") :
+    BaseTransactionManager(storagePath),TransactionRetrieverFile  {
+
+
+    override fun getSpecificTransaction(id: Int): Transaction? {
+        return transactions.find { it.id == id }
+    }
+
+    override fun getAllTransaction(): List<Transaction> {
+        return transactions.toList()
+    }
+
+    override fun getMonthlyTransactions(month: Int): List<Transaction> {
+        return transactions
+            .filter { it.date.monthValue == month }
+            .sortedByDescending { it.date }
+    }
+
+}
+
+
+
