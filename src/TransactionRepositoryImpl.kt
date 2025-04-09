aa4c3208cd838750a1f0b19ba4f32532de8bc31d
Zeyad Abdullah <zeyadgalal93@gmail.com>
2025-04-09 10:58:16 +0200

    set up a blueprint for transaction functionalities

---
+++ src/TransactionRepositoryImpl.kt
@@ -0,0 +1,15 @@
+class TransactionRepositoryImpl(
+    private val transactions: MutableList<Transaction>
+): TransactionRepository {
+
+    // function that adds a single transaction
+
+    // function that edits a single transaction
+
+    // function to view all transactions
+
+    // function to delete a single transaction
+
+    // function for monthly summary and balance report
+
+}
