3743793855aa521e97ccc8a32829372c24c668da
alaafawzy <alaaf9443@gmail.com>
2025-04-10 16:58:43 +0200

    -

--- src/file/TransactionFileWriterImpl.kt
+++ src/file/TransactionFileWriterImpl.kt
@@ -5,7 +5,8 @@ import Transaction
 class TransactionFileWriterImpl(storagePath: String = "data/transactions.csv") :
     BaseTransactionManager(storagePath), TransactionAdderFile {
 
-    override fun addTransaction(transaction: Transaction) {
+   override fun addTransaction(transaction: Transaction) {
         super.addTransaction(transaction)
     }
+
 }
