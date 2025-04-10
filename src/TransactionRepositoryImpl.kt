92a0a3680aee2fb70002d69df38847028558b27f
AimanYosofi <=>
2025-04-11 00:06:04 +0200

    refactor: implement transaction-repository methods after changing parameters to get direct inputs from the CLI

--- src/TransactionRepositoryImpl.kt
+++ src/TransactionRepositoryImpl.kt
@@ -4,58 +4,61 @@ class TransactionRepositoryImpl(
     private val transactions: MutableList<Transaction>
 ) : TransactionRepository {
 
-    override fun addTransaction(transaction: Transaction): Boolean {
-        return try {
+    override fun addTransaction(amount: Double, category: Category): Boolean {
+        if (amount <= 0)
+            return false
 
-            if (transaction.amount < 0) {
-                println("Attention: Negative transaction amount found! ")
-            }
+        val newId = if (transactions.isEmpty()) 1
+        else transactions.maxOf { it.id } + 1
 
-            val newId = if (transactions.isEmpty()) 1 else transactions.maxOf { it.id } + 1
+        val transaction = Transaction(
+            id = newId, amount = amount, type = category.type,
+            category = category, date = LocalDateTime.now()
+        )
 
-            val newTransaction = transaction.copy(id = newId)
-
-            transactions.add(newTransaction)
-            println("Transaction added with ID: $newId")
-            true
-        } catch (e: Exception) {
-            println("Error adding transaction: ${e.message ?: "something went wrong"}")
-            false
-        }
+        transactions.add(transaction)
+        return true
     }
 
-    override fun editTransaction(transaction: Transaction): Boolean {
-
-        if (transaction.id == 0) {
-            println("Error: Transaction ID can't be 0")
+    override fun editTransactionAmount(id: Int, amount: Double): Boolean {
+        if (id == 0)
             return false
-        }
 
-        val index = transactions.indexOfFirst { it.id == transaction.id }
-        return if (index != -1) {
-            val existingTransaction = transactions[index]
+        if (amount <= 0)
+            return false
 
-            if (existingTransaction == transaction) {
-                println("No changes found for transaction ID ${transaction.id}")
-                true
-            } else {
-                transactions[index] = transaction
-                println("Transaction with ID ${transaction.id} updated successfully")
-                true
-            }
-        } else {
-            println("Error: Transaction with ID ${transaction.id} could not be found")
-            false
-        }
+        val transactionIndex = findTransactionIndexById(id)
+        if (transactionIndex == -1)
+            return false
+
+        transactions[transactionIndex] = transactions[transactionIndex].copy(amount = amount)
+        return true
+    }
+
+    override fun editTransactionCategory(id: Int, category: Category): Boolean {
+        if (id == 0)
+            return false
+
+        val transactionIndex = findTransactionIndexById(id)
+        if (transactionIndex == -1)
+            return false
+
+        transactions[transactionIndex] = transactions[transactionIndex].copy(category = category)
+        return true
     }
 
     override fun deleteTransaction(id: Int): Boolean {
-        val index = transactions.indexOfFirst { it.id == id }
-        return if (index != -1) {
-            transactions.removeAt(index)
-            true
-        } else {
-            false
-        }
+        val transactionIndex = findTransactionIndexById(id)
+        if (transactionIndex == -1)
+            return false
+
+        transactions.removeAt(transactionIndex)
+        return true
+    }
+
+    override fun findTransactionIndexById(id: Int): Int {
+        if (id == 0)
+            return -1
+        return transactions.indexOfFirst { it.id == id }
     }
 }
